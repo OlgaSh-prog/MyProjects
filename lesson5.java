ebe198f041b18c2e2bffa02792bbc513adb73e43
Olga <66097300+OlgaSh-prog@users.noreply.github.com>
2020-05-30 22:12:23 -0400

    first commit

---
+++ lesson5.java
@@ -0,0 +1,22 @@
+public class lesson5 {
+
+    public static void main(String[] args) {
+
+        String text = "       I l__^^^^^e_ar)n j=====a-V*a";
+
+        //remove leading and trailing spaces.
+        text = text.trim();
+
+        //symbols replacement
+        text = text.replace("_","");
+        text = text.replace("^","");
+        text = text.replace(")","");
+        text = text.replace("=","");
+        text = text.replace("-","");
+        text = text.replace("*","");
+        text = text.replace("V","v");
+        
+        System.out.println(text +"!");
+    }
+}
+
